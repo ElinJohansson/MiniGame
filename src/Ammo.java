f308bb5acf3874f3c0a841d2fa01f731a4b6160d
Joan Ytterborg <j.ytterborg@gmail.com>
2018-02-28 14:05:20 +0100

    Add changes in class Ammo, Weapon and Direction.

--- src/Ammo.java
+++ src/Ammo.java
@@ -1,2 +1,26 @@
 public class Ammo {
+    private char shot = '¤';
+    private Position position;
+    private int direction;
+
+    public Ammo(Position weaponPosition, int weaponDirection){
+        position =weaponPosition;
+        direction=weaponDirection;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public char getShot() {
+        return shot;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Weapon {
+    private char revolver = '\u00AE';
+    private Position position;
+    private int direction;
+    public List<Ammo> shotsFired = new ArrayList<>();
+
+    public Weapon(Position playerPosition){
+        position =playerPosition;
+        direction=Direction.RIGHT_X;
+    }
+
+    public char getRevolver() {
+        return revolver;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position playerPosition) {
+        position = playerPosition;
+    }
+
+    public void shoot(){
+        Ammo newShot = new Ammo(position,direction);
+        shotsFired.add(newShot);
+    }
 }
