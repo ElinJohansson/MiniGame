606b586fde7abebca2bb1645e4e6152eac61cd83
Madeleine Karlsson <madeleine.karlsson@outlook.com>
2018-02-28 12:21:49 +0100

    Maddes ändringar på Position

--- src/Position.java
+++ src/Position.java
@@ -1,2 +1,36 @@
 public class Position {
+
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getPositionX(){
+        return this.x;
+
+    }
+
+    public int getPositionY() {
+        return this.y;
+    }
+
+    public void setPositionX(int x){
+        this.x = x;
+
+    }
+
+    public void setPositionY(int y){
+        this.y = y;
+
+    }
+
+    public int hitWall (int x, int y){
+        if( x==?);
+
+
+    }
 }
