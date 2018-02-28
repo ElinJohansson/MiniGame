952488f1e34bd0b89065fd4acaeb907b0e13c895
Madeleine Karlsson <madeleine.karlsson@outlook.com>
2018-02-28 14:29:34 +0100

    Maddes ändringar på Enemy-class

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,29 @@
 public class Enemy {
+
+    char enemyFace = '\u00DF';
+    Position enemyPosition;
+
+
+    // returns the position of the enemy to the map to be drawn on the gameboard
+    public int enemy (Position enemyPosition, Map map ){
+        return (map.gameBoard[enemyPosition.getPositionX()][enemyPosition.getPositionY()]);
+    }
+
+    public int moveEnemy(){
+        
+
+    }
+
+
+    public int checkCollisionOtherEnemy(){
+
+
+    }
+
+
 }
+
+
+
+
+
